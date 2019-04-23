4f3a6b361d9837a40714544604a729ce1bc6027a
Stefan Ziegler <edi.gonzales@gmail.com>
2019-04-23 18:51:12 +0200

    wip

---
+++ src/main/java/ch/so/agi/avgbs/controllers/UploadController.java
@@ -0,0 +1,79 @@
+package ch.so.agi.avgbs.controllers;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    
+    private static String FOLDER_PREFIX = "avgbs_checkservice_";
+
+    @Value("${app.pathToUploadFolder}")
+    private String pathToUploadFolder;
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String upload() {
+        
+        return "upload";
+    }
+    
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> uploadFile(
+            @RequestParam(name = "file", required = true) MultipartFile uploadFile) {
+     
+        try {
+            // Get the file name.
+            String fileName = uploadFile.getOriginalFilename();
+            
+            // If the upload button was pushed w/o choosing a file,
+            // we just redirect to the starting page.
+            if (uploadFile.getSize() == 0 || fileName.trim().equalsIgnoreCase("") || fileName == null) {
+                log.warn("No file was uploaded. Redirecting to starting page.");
+    
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Location", servletContext.getContextPath() + "/upload");
+                return new ResponseEntity<String>(headers, HttpStatus.FOUND);
+            }
+    
+            // Build the local file path.
+            Path tmpDirectory = Files.createTempDirectory(Paths.get(pathToUploadFolder), FOLDER_PREFIX);
+            Path uploadFilePath = Paths.get(tmpDirectory.toString(), fileName);
+    
+            // Save the file locally.
+            byte[] bytes = uploadFile.getBytes();
+            Files.write(uploadFilePath, bytes);
+    
+            return ResponseEntity.badRequest().contentType(MediaType.parseMediaType("text/plain")).body("fubar");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().contentType(MediaType.parseMediaType("text/plain")).body(e.getMessage());
+        }
+    }
+
+
+}
